9d4781f0259cb51c0daa20cc4d71d0da8ea27b52
youngguyz <2281300996@qq.com>
2022-07-03 14:20:22 +0800

    feat:dinfineProp

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
-    我是 App 组件
+    <h1>我是 App 组件</h1>
   </div>
-  <Son/>
+  <Son :house="house"/>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
 import Son from './components/Son.vue';
 
+const house=ref('大别墅')
 </script>
 
 <style scoped>
--- src/components/Son.vue
+++ src/components/Son.vue
@@ -2,9 +2,24 @@
   <div>
     <h2>子组件</h2>
   </div>
+  <div>父亲的礼物：{{house}}{{car}}</div>
 </template>
 
 <script setup lang="ts">
+// 非 ts 语法 接受父组件传参
+// defineProps({
+//   house:{
+//     type:String,
+//     required:true
+//   }
+// })
+
+// ts 写法
+// 使用ts的泛型指令props类型
+defineProps<{house:string,car?:string}>()
+//----------------------------------------------------------
+
+
 
 </script>
 
