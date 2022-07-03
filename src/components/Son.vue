f6d44e9daaa59c431d3b139c3b27923f41534087
youngguyz <2281300996@qq.com>
2022-07-03 14:59:51 +0800

    feat:defineEmits

--- src/components/Son.vue
+++ src/components/Son.vue
@@ -3,6 +3,7 @@
     <h2>子组件</h2>
   </div>
   <div>父亲的礼物：{{house}}{{car}}</div>
+  <button @click="change">回增礼物</button>
 </template>
 
 <script setup lang="ts">
@@ -18,8 +19,16 @@
 // 使用ts的泛型指令props类型
 defineProps<{house:string,car?:string}>()
 //----------------------------------------------------------
+// const emit=defineEmits(['giveGift'])
+// const change=()=>{
+//   emit('giveGift','儿媳妇')
+// }
 
-
+// ts defineEmits
+const emit=defineEmits<{(e:'giveGift',gift:string):void}>()
+const change=()=>{
+  emit('giveGift','一个儿媳妇')
+}
 
 </script>
 
